b8700f6329f238dfc2c05ba99d371088f3560225
David Weid II <david.weid.2@gmail.com>
2024-02-06 14:13:37 -0500

    initialize astro project w alpinejs + tailwind

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import alpinejs from '@astrojs/alpinejs';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  prefetch: {
+    prefetchAll: true
+  },
+  integrations: [alpinejs(), tailwind()]
+});
