e640db3bb287d8bb39e90566ddc6ca30d9a5a061
David Weid II <david.weid.2@gmail.com>
2024-02-07 17:37:08 -0500

    define cookbookCollection + render it
    render cookbook list page with collection items and render cookbook/recipe pages (single pages)

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,42 +1,25 @@
-// // 1. Import utilities from `astro:content`
-// import { z, defineCollection } from "astro:content";
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
 
-// // 2. Define a `type` and `schema` for each collection
-// const blogCollection = defineCollection({
-//   type: "content", // v2.5.0 and later
-//   schema: z.object({
-//     title: z.string(),
-//     tags: z.array(z.string()),
-//     image: z.string().optional()
-//   })
-// });
+// 2. Define a `type` and `schema` for each collection
+const cookbookCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date().optional(),
+    description: z.string().optional(),
+    author: z.string().optional(),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string()
+      })
+      .optional(),
+    tags: z.array(z.string()).optional()
+  })
+});
 
-// // 3. Export a single `collections` object to register your collection(s)
-// export const collections = {
-//   blog: blogCollection
-// };
-
-// const blogCollection = defineCollection({
-//   type: "content",
-//   schema: z.object({
-//     /* ... */
-//   })
-// });
-// const newsletter = defineCollection({
-//   type: "content",
-//   schema: z.object({
-//     /* ... */
-//   })
-// });
-// const authors = defineCollection({
-//   type: "data",
-//   schema: z.object({
-//     /* ... */
-//   })
-// });
-
-// export const collections = {
-//   blog: blogCollection,
-//   newsletter: newsletter,
-//   authors: authors
-// };
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  cookbook: cookbookCollection
+};
