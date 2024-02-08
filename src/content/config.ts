6fdb8227094f13b5fceddcc6fb25d71d61e9476c
David Weid II <david.weid.2@gmail.com>
2024-02-08 16:56:28 -0500

    reduce typescript errors

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,18 +6,18 @@ const cookbookCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
-    pubDate: z.date().optional(),
-    description: z.string().optional(),
-    author: z.string().optional(),
-    image: z
-      .object({
-        url: z.string(),
-        alt: z.string()
+    pubDate: z.nullable(z.date()),
+    description: z.nullable(z.string()),
+    author: z.nullable(z.string()),
+    image: z.nullable(
+      z.object({
+        url: z.nullable(z.string()),
+        alt: z.nullable(z.string())
       })
-      .optional(),
-    tags: z.array(z.string()).optional(),
-    ingredients: z.array(z.string()).optional(),
-    instructions: z.array(z.string()).optional()
+    ),
+    tags: z.nullable(z.array(z.string())),
+    ingredients: z.nullable(z.array(z.string())),
+    instructions: z.nullable(z.array(z.string()))
   })
 });
 
