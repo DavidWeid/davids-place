f65eb038c9fd58607cc19e09b25e1dd5b5560416
David Weid II <david.weid.2@gmail.com>
2024-02-22 13:09:26 -0500

    Test out astro built-in image processing

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,26 +4,27 @@ import { z, defineCollection } from 'astro:content';
 // 2. Define a `type` and `schema` for each collection
 const cookbookCollection = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    source: z.nullable(z.string()),
-    pubDate: z.nullable(z.date()),
-    author: z.string(),
-    image: z.nullable(
-      z.object({
-        url: z.nullable(z.string()),
-        alt: z.nullable(z.string())
-      })
-    ),
-    tags: z.array(z.string()),
-    servings: z.string(),
-    prepTime: z.string(),
-    cookTime: z.string(),
-    totalTime: z.string(),
-    ingredients: z.array(z.string()),
-    instructions: z.array(z.string())
-  })
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      source: z.nullable(z.string()),
+      pubDate: z.nullable(z.date()),
+      author: z.string(),
+      image: z.nullable(
+        z.object({
+          url: image(),
+          alt: z.string()
+        })
+      ),
+      tags: z.array(z.string()),
+      servings: z.string(),
+      prepTime: z.string(),
+      cookTime: z.string(),
+      totalTime: z.string(),
+      ingredients: z.array(z.string()),
+      instructions: z.array(z.string())
+    })
 });
 
 // 3. Export a single `collections` object to register your collection(s)
