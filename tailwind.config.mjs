699b35c8443ebb129741eb511275480c55a614ee
David Weid II <david.weid.2@gmail.com>
2024-03-01 13:35:35 -0500

    add font files + update tailwind to tighten usage

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
@@ -8,7 +10,26 @@ export default {
       laptop: '1024px',
       desktop: '1400px'
     },
+    fontWeight: {
+      thin: 100,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      bold: 700,
+      black: 900,
+      'mono-thin': 100,
+      'mono-extralight': 200,
+      'mono-light': 300,
+      'mono-normal': 400,
+      'mono-medium': 500,
+      'mono-semibold': 600,
+      'mono-bold': 700
+    },
     extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+        mono: ['Roboto Mono Variable', ...defaultTheme.fontFamily.mono]
+      },
       colors: {
         dark: '#13151a',
         light: '#f5f5f5',
