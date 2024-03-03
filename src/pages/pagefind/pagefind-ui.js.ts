3a319ee7fc078d43305f35100ec54966a7bf05d5
David Weid II <david.weid.2@gmail.com>
2024-03-03 11:54:08 -0500

    add search

---
+++ src/pages/pagefind/pagefind-ui.js.ts
@@ -0,0 +1,7 @@
+import type { APIContext } from 'astro';
+
+export async function GET({}: APIContext) {
+  return new Response(
+    'export const search = () => { return  { results: [] } }'
+  );
+}
