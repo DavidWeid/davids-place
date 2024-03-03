9963dbdf67c52812b42c635267a7217dfa3ea5ee
David Weid II <david.weid.2@gmail.com>
2024-03-03 12:01:48 -0500

    Revert "add search"
    This reverts commit 3a319ee7fc078d43305f35100ec54966a7bf05d5.

--- src/pages/pagefind/pagefind-ui.js.ts
+++
@@ -1,7 +0,0 @@
-import type { APIContext } from 'astro';
-
-export async function GET({}: APIContext) {
-  return new Response(
-    'export const search = () => { return  { results: [] } }'
-  );
-}
