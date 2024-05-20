0441721948e0fbb6dc27682f4b390d2ff1c63123
David Weid II <david.weid.2@gmail.com>
2024-05-20 11:54:33 -0400

    Update .prettierrc.mjs

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,13 +1,14 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ['prettier-plugin-astro'],
   overrides: [
     {
-      files: "*.astro",
+      files: '*.astro',
       options: {
-        parser: "astro",
+        parser: 'astro',
       },
     },
   ],
+  singleQuote: true,
 };
