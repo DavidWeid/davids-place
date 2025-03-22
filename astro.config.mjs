6294d7490e600aa656166f63c1c0da552130128c
David J Weid II <david.weid.2@gmail.com>
2025-03-22 15:14:52 -0400

    Upgrade to Tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,18 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: defineConfig({
-    ssr: {
-      external: ['tailwind.config.mjs'],
-    },
-  }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: 'https://davidweid.github.io',
   base: '/davids-place',
   trailingSlash: 'always',
   prefetch: {
     prefetchAll: true,
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [mdx(), sitemap()],
 });
