71a59c55aae1ce1f8ed17ca21726cb4c2dfa8cfe
David Weid II <david.weid.2@gmail.com>
2025-09-04 23:01:46 -0400

    fix cloud function wrapper

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,13 @@
-const { onRequest } = require('firebase-functions/v2/https');
-const { setGlobalOptions } = require('firebase-functions/v2');
+import { onRequest } from 'firebase-functions/v2/https';
+import { setGlobalOptions } from 'firebase-functions/v2';
+import { handler } from '../dist/server/entry.mjs';
 
 // Set global options
 setGlobalOptions({
   region: 'us-east1',
 });
 
-// Import the Astro SSR handler
-const handler = require('../dist/server/entry.mjs');
-
 // Export the server function
-exports.server = onRequest(async (req, res) => {
-  return handler.handler(req, res);
+export const server = onRequest(async (req, res) => {
+  return handler(req, res);
 });
