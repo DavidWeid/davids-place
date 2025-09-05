5943dc712eab622a35e9719fdce45aecdd8b4c2d
David Weid II <david.weid.2@gmail.com>
2025-09-05 09:02:08 -0400

    fix cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,14 @@
-import { onRequest } from 'firebase-functions/v2/https';
-import { setGlobalOptions } from 'firebase-functions/v2';
+import functions from 'firebase-functions';
 import { handler } from '../dist/server/entry.mjs';
 
-// Set global options
-setGlobalOptions({
-  region: 'us-east1',
-});
-
-// Export the server function
-export const server = onRequest(async (req, res) => {
-  return handler(req, res);
-});
+// Export the server function using Firebase Functions v1
+export const server = functions
+  .region('us-east1')
+  .https.onRequest(async (req, res) => {
+    try {
+      return await handler(req, res);
+    } catch (error) {
+      console.error('Function error:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
