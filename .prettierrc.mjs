997c77d766d3bb8b4181f2fbfa8cd180f67084aa
David J Weid II <david.weid.2@gmail.com>
2025-11-09 15:11:53 -0500

    Format files

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,15 +1,14 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
- plugins: ['prettier-plugin-astro'],
- overrides: [
-  {
-   files: '*.astro',
-   options: {
-    parser: 'astro',
-   },
-  },
- ],
- singleQuote: true,
- tabWidth: 1,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  singleQuote: true,
 };
