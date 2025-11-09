d4c9b9db78c20f48f7b3432c7230a486bed70c62
David J Weid II <david.weid.2@gmail.com>
2025-11-09 00:27:24 -0500

    Start homepage redesign (hero)

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,14 +1,15 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ['prettier-plugin-astro'],
-  overrides: [
-    {
-      files: '*.astro',
-      options: {
-        parser: 'astro',
-      },
-    },
-  ],
-  singleQuote: true,
+ plugins: ['prettier-plugin-astro'],
+ overrides: [
+  {
+   files: '*.astro',
+   options: {
+    parser: 'astro',
+   },
+  },
+ ],
+ singleQuote: true,
+ tabWidth: 1,
 };
